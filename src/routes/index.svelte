c38b8cf114227f9bfcfefd90d216fb6ddb9549e3
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2022-02-12 15:49:12 -0500

    Page de maintenance

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '../style.css'
+</script>
+
+<div class="fs" id="make-me-cool">
+    <img src="/temp.svg" class="si">
+</div>
+
+<div class="texte">
+    Site en maintenance! <br>
+    <small><small>De retour bientôt!</small></small>
+</div>
+
+
